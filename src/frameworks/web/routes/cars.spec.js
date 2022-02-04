511d2f9004f1b036ab33ae8f883cb513181fa91f
Majid Qureshi <mpq1990@gmail.com>
2022-02-04 19:18:05 +0100

    fix: faling spec

--- src/frameworks/web/routes/cars.spec.js
+++ src/frameworks/web/routes/cars.spec.js
@@ -4,7 +4,7 @@ const supertest = require('supertest');
 const fakeRepository = {
   carRepository: {
     add: (car) => Promise.resolve(car),
-    getAll: () => Promise.resolve([]),
+    getAll: () => Promise.resolve([{ id: 1 }]),
   },
 };
 
@@ -12,7 +12,10 @@ const app = require('../app')(fakeRepository);
 
 describe('GET /api/cars', () => {
   it('should return 200', (done) => {
-    supertest(app).get('/api/cars').expect(200, done);
+    supertest(app)
+      .get('/api/cars')
+      .expect([{ id: 1 }])
+      .expect(200, done);
   });
 
   it('should return 404 in case of wrong url', (done) => {
